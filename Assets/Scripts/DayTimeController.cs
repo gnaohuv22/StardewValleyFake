30777c81db27b0cc498d3018e73e3a182fbdfe52
ThaiTrinh1234 <thaitnhe176087@fpt.edu.vn>
2024-06-13 12:15:04 +0700

    complete time Agents of seed spawning

--- Assets/Scripts/DayTimeController.cs
+++ Assets/Scripts/DayTimeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ using UnityEngine.UI;
 public class DayTimeController : MonoBehaviour
 {
     const float secondsInDay = 86400f;
+    const float phaseLength = 900f; //15 min 
 
     [SerializeField] Color nightLightColor;
     [SerializeField] AnimationCurve nightTimeCurve;
@@ -15,9 +17,32 @@ public class DayTimeController : MonoBehaviour
 
     float time;
     [SerializeField] float timeScale = 60f;
+    [SerializeField] float startAtTime = 28800f; //second
+
     [SerializeField] Text text;
     [SerializeField] Light2D globalLight;
     private int days;
+
+    List<TimeAgent> agents;
+
+    private void Awake()
+    {
+        agents = new List<TimeAgent> ();
+    }
+
+    private void Start()
+    {
+        time = startAtTime;
+    }
+
+    public void Subscrbe(TimeAgent timeAgent)
+    {
+        agents.Add(timeAgent);
+    }
+    public void Unsubscrbe(TimeAgent timeAgent)
+    {
+        agents.Remove(timeAgent);
+    }
 
     float Hours
     {
@@ -31,17 +56,47 @@ public class DayTimeController : MonoBehaviour
     private void Update()
     {
         time += Time.deltaTime * timeScale;
-        int hh = (int)Hours;
-        int mm = (int)Minutes;
-        text.text = hh.ToString("00") + ":" + mm.ToString("00");
-        float v = nightTimeCurve.Evaluate(Hours);
-        Color c = Color.Lerp(dayLightColor, nightLightColor, v);
-        globalLight.color = c;
+        TimeValueCalculation();
+        DayLight();
         if (time > secondsInDay)
         {
             NextDay();
         }
+
+        TimeAgents();
     }
+
+    private void TimeValueCalculation()
+    {
+        int hh = (int)Hours;
+        int mm = (int)Minutes;
+        text.text = hh.ToString("00") + ":" + mm.ToString("00");
+    }
+
+    private void DayLight()
+    {
+        float v = nightTimeCurve.Evaluate(Hours);
+        Color c = Color.Lerp(dayLightColor, nightLightColor, v);
+        globalLight.color = c;
+    }
+
+    int oldPhase = 0;
+    private void TimeAgents()
+    {
+        int currentPhase = (int)(time / phaseLength);
+        if (oldPhase != currentPhase)
+        {
+            oldPhase = currentPhase;
+            for (int i = 0; i < agents.Count; i++)
+            {
+                agents[i].Invoke();
+            }
+
+        }
+
+
+    }
+
 
     private void NextDay()
     {
