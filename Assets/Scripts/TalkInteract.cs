527897bcad133acbc8ee8fd0085538db892063d6
ThaiTrinh1234 <thaitnhe176087@fpt.edu.vn>
2024-06-14 03:43:35 +0700

    Done #15: Add Dialog chat with NPC

--- Assets/Scripts/TalkInteract.cs
+++ Assets/Scripts/TalkInteract.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class TalkInteract : Interactable
 {
+    [SerializeField] DialogueContainer dialogue;
     public override void Interact(Character character)
     {
-        Debug.Log("You talked with me.");
+        GameManager.instance.dialogueSystem.Initialize(dialogue);
     }
 }
