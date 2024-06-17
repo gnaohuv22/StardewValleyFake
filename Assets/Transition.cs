ea3b807b5517b06ba331a1e99b991fe1dd047c0f
Githnewbie <anhpvhe176182@fpt.edu.vn>
2024-06-18 05:00:31 +0700

    vid16: Basic Transition

---
+++ Assets/Transition.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum TransitionType
+{
+    Warp,
+    Scene
+}
+public class Transition : MonoBehaviour
+{
+    [SerializeField] TransitionType transitionType;
+    [SerializeField] string sceneNameToTransition;
+
+    Transform destination;
+
+    void Start()
+    {
+        destination = transform.GetChild(1);
+    }
+    internal void InitiateTransition(Transform toTransition)
+    {
+        switch (transitionType)
+        {
+            case TransitionType.Warp:
+                toTransition.position = new Vector3(
+            destination.position.x,
+            destination.position.y,
+            toTransition.position.z);
+                break;
+            case TransitionType.Scene:
+                SceneManager.LoadScene(sceneNameToTransition);
+                break;
+        }
+
+    }
+
+}
