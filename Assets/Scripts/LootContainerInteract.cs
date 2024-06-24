664d4816fac5e9920616ee02acd5bf6366cfcea7
Githnewbie <anhpvhe176182@fpt.edu.vn>
2024-06-25 04:46:59 +0700

    vid 21: music and audio manager

--- Assets/Scripts/LootContainerInteract.cs
+++ Assets/Scripts/LootContainerInteract.cs
@@ -7,13 +7,15 @@ public class LootContainerInteract : Interactable
     [SerializeField] GameObject closedChest;
     [SerializeField] GameObject openedChest;
     [SerializeField] bool opened;
+    [SerializeField] AudioClip onOpenAudio;
     public override void Interact(Character character)
     {
         if (!opened)
         {
+            opened = true;
             closedChest.SetActive(false);
             openedChest.SetActive(true);
-            opened = true;
+            AudioManager.instance.Play(onOpenAudio);
         }
     }
 }
